8394d9cd9eb059697b1ff52dc676a4af3c02563f
katyopa <76550508+katyopa@users.noreply.github.com>
2021-05-25 17:28:13 +0300

    functions suck

--- functions.py
+++ functions.py
@@ -7,6 +7,24 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 import os
 from collections import Counter
+from tld import get_tld
+import tkinter as tk
+from database import select_from_db
+import argparse
+
+
+def parse_tagcounter_args():
+    """Create the arguments parser, adds 2 arguments."""   
+    my_parser = argparse.ArgumentParser(prog='tagcounter',           # create the arguments parser
+                                        description='Count tags of an html-file')
+    
+    my_parser.add_argument('--get',                                  # add the --get argument
+                           type=str,
+                           help='get tags and their counts')
+    my_parser.add_argument('--view',                                 # add the --view argument
+                           help='fetch data from the database')
+
+    return my_parser.parse_args()                                    # execute the parse_args() method
 
 
 def add_protocol_to_url(url):
@@ -17,46 +35,59 @@ def add_protocol_to_url(url):
     return url
 
 
-# parse yaml-file
 def parse_yaml_file():
+    """Parse yaml-file."""
     a_yaml_file = open(r'C:\Users\Katsiaryna_Lastouska\Documents\Learn To Code Python\learn-to-code-with-python-incomplete\learn-to-code-with-python\bullshit-v.1.0\synonyms.yaml')
     parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
     
     return parsed_yaml_file
 
 
-# unpickle results from DB
+def get_synonym(url):
+    """Check if synonym exists in synonyms.yaml, then use it to build an object."""
+    synonyms = parse_yaml_file() 
+
+    return synonyms.get(url, url)             # returns value or default value if doesn't exist in yaml file
+
+
+def get_domain_site_by_url(url):
+    """Extract 2nd level domain name: 'google'."""
+    site = get_tld(url, as_object=True)
+    domain = site.domain
+
+    return domain
+
+def pickle_tag_dict(tag_dict):
+    """Pickle the Python object: convert the dictionary into a byte stream"""
+    pickled_tag_dict = pickle.dumps(tag_dict, protocol=None, fix_imports=True)
+
+    return pickled_tag_dict
+
+
 def unpickle_db_results(results):
+    """Unpickle results from DB."""
     unpickled_list = pickle.loads(results, fix_imports=True,
                                   encoding="ASCII", errors="strict",
                                   buffers=None)
-    print(unpickled_list)
     
     return unpickled_list
 
 
-class ContaTags:
+class CountTags:
 
     def __init__(self, url):
         self.url = url
         self.arquivos = []
         self.tags = []
-        self.Lista = []
 
-    def run(self):
-        # check if we enter a URL without the protocol
-        global url        # переменную сделаем глобальной
-        url = self.url                  # declared a variable
-        if not urlparse(url).scheme:
-            url = 'http://' + url
-
-        logging.info(url)  # use declared url variable
-        # download html content: request - response
-        response = urllib.request.urlopen(url)  # use url variable
-        webContent = response.read()
-        # write to a file
-        f = open('webpage.html', 'wb')
-        f.write(webContent)
+    def count_tags(self):
+        """Count tags of a webpage."""
+        url = add_protocol_to_url(self.url)      # add protocol to url if needed
+        logging.info(url)                        # use declared url variable
+        response = urllib.request.urlopen(url)   # download html content: request - response
+        web_content = response.read()
+        f = open('webpage.html', 'wb')           # write to a file
+        f.write(web_content)
         f.close
 
         class MyHTMLParser(HTMLParser):
@@ -72,21 +103,85 @@ class ContaTags:
         for file in os.listdir(path=os.getcwd()):
             if file.endswith(".html"):
                 self.arquivos.append(file)
-                caminho = os.getcwd() + "\\" + file
-                doc = open(caminho, "r", encoding="utf8")
+                path = os.getcwd() + "\\" + file
+                doc = open(path, "r", encoding="utf8")
                 soup = BeautifulSoup(doc, 'html.parser')
                 count = []
                 parser.feed(soup.prettify())
                 self.tags.append(count)
 
-        z = []
+        z = []                                         # a list with the collections.Counter Object
 
         for i, item in enumerate(self.tags):
             z.append(Counter(self.tags[i]))
 
-        # print(z)  # a list with a the collections.Counter Object
-        # list with the collections.Counter Object to a dict
-        tag_dict = dict(z[0])
-        # print(tag_dict)
+        tag_dict = dict(z[0])                          # list with the collections.Counter Object to a dict
 
         return tag_dict
+
+
+class GuiWindow:
+    def start(self):
+        window = tk.Tk()
+        window.title("Welcome to Tagcounter!")
+        window.geometry('600x450')
+        text = tk.Label(window, text="Enter url, please")
+        text.grid(column=1,
+                row=0,
+                columnspan=2,
+                sticky=tk.E,
+                pady=10,
+                padx=10)
+        txt = tk.Entry(window, width=30)
+        txt.focus()
+        txt.grid(column=3,
+                row=0,
+                columnspan=2,
+                sticky=tk.W+tk.E,
+                pady=10,
+                padx=10)
+        output = tk.Text(window, height=10, width=45)
+        output.grid(column=2, row=4, columnspan=3, pady=10, padx=10)
+        scroll = tk.Scrollbar(command=output.yview)
+        scroll.grid(column=5, row=4)
+        output.config(yscrollcommand=scroll.set)
+        statusbar = tk.Label(window,
+                            text="waiting for the url…",
+                            bd=1, relief=tk.SUNKEN,
+                            anchor=tk.W)
+        statusbar.grid(column=0,
+                        row=6,
+                        columnspan=5,
+                        sticky=tk.W+tk.E,
+                        pady=10,
+                        padx=10)
+
+        def load_from_db():
+            gui_user_input = txt.get()
+            gui_user_input = get_synonym(gui_user_input)              # check synonym in yaml file
+            gui_full_url = add_protocol_to_url(gui_user_input)        # add protocol to url
+            gui_results = select_from_db(gui_full_url)                # select from DB
+            gui_results = gui_results[0][0]                           # extract pickled object
+            gui_unpickled_result = unpickle_db_results(gui_results)   # unpickle results 
+            output.delete(1.0, tk.END)                                # clean the output box before insert
+            result_load_from_db = "{}: ".format(txt.get()) + str(gui_unpickled_result)  # create output
+            output.insert(tk.END, result_load_from_db)                # insert output to the textbox
+            statusbar.configure(text="I've counted tags for you!")    # update status bar
+
+        def load_from_internet():
+            gui_user_input_inet = txt.get()
+            gui_user_input_inet = get_synonym(gui_user_input_inet)
+            SampleObject = CountTags(gui_user_input_inet)
+            gui_tagcount_dict = SampleObject.count_tags()
+            result_load_from_internet = "{}: ".format(txt.get()) + str(gui_tagcount_dict)
+            output.delete(1.0, tk.END)                                # clean the output box before insert
+            output.insert(tk.END, result_load_from_internet)
+            statusbar.configure(text="I've counted tags for you!")
+
+        btn_internet = tk.Button(window,
+                                text="Download From Internet",
+                                command=load_from_internet)
+        btn_internet.grid(column=4, row=3, padx=10, pady=10, sticky=tk.E)
+        btn_db = tk.Button(window, text="Show From DB", command=load_from_db)
+        btn_db.grid(column=3, row=3, sticky=tk.W, pady=10, padx=10)
+        window.mainloop()
--- tagcounter.py
+++ tagcounter.py
@@ -1,168 +1,76 @@
-from database import engine, tagcounter_db, select_from_db
+from database import select_from_db, save_results
 import functions as fc
 import logging
-import argparse
-import pickle
-from tld import get_tld
-from datetime import datetime
 import tkinter as tk
 
 
-# add logging
-logging.basicConfig(level=logging.INFO,
-                    filename='tagcounter_log.log',
-                    filemode='a',
-                    format='%(asctime)s  - %(message)s')
-
-
-def parse_tagcounter_args():
-    """Creates the arguments parser, adds 2 arguments."""   
-    my_parser = argparse.ArgumentParser(prog='tagcounter',
-                                        description='Count tags of an html-file')
-    # Add the arguments
-    my_parser.add_argument('--get',
-                           type=str,
-                           help='get tags and their counts')
-    my_parser.add_argument('--view',
-                           help='fetch data from the database')
-
-    # Execute the parse_args() method
-    return my_parser.parse_args()
-
-
 if __name__ == "__main__":
-    args = parse_tagcounter_args()
+    logging.basicConfig(level=logging.INFO,                    # add logging
+                        filename='tagcounter_log.log',
+                        filemode='a',
+                        format='%(asctime)s  - %(message)s')
+    args = fc.parse_tagcounter_args()
 
+    parsed_yaml_file = fc.parse_yaml_file()                    # parse yaml-file
 
-parsed_yaml_file = fc.parse_yaml_file()  # parse yaml-file
+    if args.get is not None:                                   # tagcounter --get url
+        url_get = fc.get_synonym(args.get)
+        Sample = fc.CountTags(url_get)
+        tagcount_dict = Sample.count_tags()                    # returns a dictionary object
+        pickled_tag_dict = fc.pickle_tag_dict(tagcount_dict)   # pickle dict to write to db
+        full_url_get = fc.add_protocol_to_url(url_get)
+        domain = fc.get_domain_site_by_url(full_url_get)       # extract 2nd level domain name
+        save_results(full_url_get, domain, pickled_tag_dict)   # save results to db
+        print(tagcount_dict)
 
-if args.get is not None:  # tagcounter --get url
-    # check if synonym exists in synonyms.yaml, then use it to build an object
-    if args.get in parsed_yaml_file.keys():
-        Sample = fc.ContaTags(parsed_yaml_file[args.get])
-    else:
-        Sample = fc.ContaTags(args.get)
+    elif args.view is not None:                                # tagcounter --view url
+        url_view = fc.get_synonym(args.view)
+        full_url = fc.add_protocol_to_url(url_view)
+        results = select_from_db(full_url)                     # connect to DB and execute select
+        results = results[0][0]                                # extract pickled object from the results list
+        print(fc.unpickle_db_results(results))                 # unpickle results from DB
 
-    cmd_tagcount_dict = Sample.run()  # returns a dictionary object
-    # Pickle the Python object: convert the object into a byte stream
-    pickled_cmd_tagcount_dict = pickle.dumps(cmd_tagcount_dict,
-                                             protocol=None,
-                                             fix_imports=True)
-    # extract 2nd level domain name: 'google' (передаем туда весь урл (ошибка))
-    site = get_tld(fc.url, as_object=True)
-    domain = site.domain
-    # check data in tags.db: update if exists, insert if not
-    connection = engine.connect()
-    check = tagcounter_db.select().where(tagcounter_db.c.url == fc.url)
-    check_result = connection.execute(check).scalar()  # returns True / False
-    if check_result:
-        update_data = tagcounter_db.update().where(tagcounter_db.c.url
-                                                   == fc.url).values(
-                                                   site_name=domain,
-                                                   url=fc.url,
-                                                   date_time=datetime.now(),
-                                                   tagcount=pickled_cmd_tagcount_dict)
-        connection.execute(update_data)
-        print("Data successfully updated!")
-    else:
-        insert_data = tagcounter_db.insert().values(
-                      site_name=domain,
-                      url=fc.url,
-                      date_time=datetime.now(),
-                      tagcount=pickled_cmd_tagcount_dict)
-        connection.execute(insert_data)
-        print("Data successfully loaded into Database!")
+    elif args.view is None and args.get is None:               # tagcounter
+        NewGuiWindow = fc.GuiWindow()
+        NewGuiWindow.start()
+        # window = tk.Tk()
+        # window.title("Welcome to Tagcounter!")
+        # window.geometry('600x450') 
+        # text = tk.Label(window, text="Enter url, please")
+        # text.grid(column=1,
+        #         row=0,
+        #         columnspan=2,
+        #         sticky=tk.E,
+        #         pady=10,
+        #         padx=10)
+        # txt = tk.Entry(window, width=30)
+        # txt.focus()
+        # txt.grid(column=3,
+        #         row=0,
+        #         columnspan=2,
+        #         sticky=tk.W+tk.E,
+        #         pady=10,
+        #         padx=10)
+        # btn_internet = tk.Button(window,
+        #                         text="Download From Internet",
+        #                         command=load_from_internet)
+        # btn_internet.grid(column=4, row=3, padx=10, pady=10, sticky=tk.E)
+        # btn_db = tk.Button(window, text="Show From DB", command=load_from_db)
+        # btn_db.grid(column=3, row=3, sticky=tk.W, pady=10, padx=10)
+        # output = tk.Text(window, height=10, width=45)
+        # output.grid(column=2, row=4, columnspan=3, pady=10, padx=10)
+        # scroll = tk.Scrollbar(command=output.yview)
+        # scroll.grid(column=5, row=4)
+        # output.config(yscrollcommand=scroll.set)
+        # statusbar = tk.Label(window,
+        #                     text="waiting for the url…",
+        #                     bd=1, relief=tk.SUNKEN,
+        #                     anchor=tk.W)
+        # statusbar.grid(column=0,
+        #                 row=6,
+        #                 columnspan=5,
+        #                 sticky=tk.W+tk.E,
+        #                 pady=10,
+        #                 padx=10)
+        # window.mainloop()
 
-    connection.close()
-    print(cmd_tagcount_dict)
-
-elif args.view is not None:  # tagcounter --view url
-    # check if synonym exists in synonyms.yaml, then use it
-    user_input = args.view
-    if user_input in parsed_yaml_file.keys():
-        user_input = parsed_yaml_file[user_input]
-    else:
-        user_input
-
-    # check if we enter an URL without the protocol, if we don't, add protocol
-    full_url = fc.add_protocol_to_url(user_input)
-    results = select_from_db(full_url)  # connect to DB and execute select
-    # print(results)
-    results = results[0][0]  # extract pickled object from the results list
-    fc.unpickle_db_results(results)  # unpickle results from DB
-
-
-elif args.view is None and args.get is None:  # tagcounter
-    def load_from_db():
-        gui_user_input = txt.get()
-        # print(user_input)
-        if gui_user_input in parsed_yaml_file.keys():
-            gui_user_input = parsed_yaml_file[gui_user_input]
-        else:
-            gui_user_input
-
-        # add protocol to url
-        gui_full_url = fc.add_protocol_to_url(gui_user_input)
-        gui_results = select_from_db(gui_full_url)  # select from DB
-        # print(results)
-        gui_results = gui_results[0][0]  # extract pickled object
-        gui_unpickled_result = fc.unpickle_db_results(gui_results) # unpickle results 
-        output.delete(1.0, tk.END)  # clean the output box before insert
-        result_load_from_db = "{}: ".format(txt.get()) + str(gui_unpickled_result)
-        output.insert(tk.END, result_load_from_db)
-        statusbar.configure(text="I've counted tags for you!")
-
-    def load_from_internet():
-        gui_user_input_inet = txt.get()
-        if gui_user_input_inet in parsed_yaml_file.keys():
-            SampleObject = fc.ContaTags(parsed_yaml_file[gui_user_input_inet])
-        else:
-            SampleObject = fc.ContaTags(gui_user_input_inet)
-
-        # print(gui_user_input_inet)
-        gui_tagcount_dict = SampleObject.run()
-        result_load_from_internet = "{}: ".format(txt.get()) + str(gui_tagcount_dict)
-        output.delete(1.0, tk.END)  # clean the output box before insert
-        output.insert(tk.END, result_load_from_internet)
-        statusbar.configure(text="I've counted tags for you!")
-
-    window = tk.Tk()
-    window.title("Welcome to Tagcounter!")
-    window.geometry('600x450')
-    text = tk.Label(window, text="Enter url, please")
-    text.grid(column=1,
-              row=0,
-              columnspan=2,
-              sticky=tk.E,
-              pady=10,
-              padx=10)
-    txt = tk.Entry(window, width=30)
-    txt.focus()
-    txt.grid(column=3,
-             row=0,
-             columnspan=2,
-             sticky=tk.W+tk.E,
-             pady=10,
-             padx=10)
-    btn_internet = tk.Button(window,
-                             text="Download From Internet",
-                             command=load_from_internet)
-    btn_internet.grid(column=4, row=3, padx=10, pady=10, sticky=tk.E)
-    btn_db = tk.Button(window, text="Show From DB", command=load_from_db)
-    btn_db.grid(column=3, row=3, sticky=tk.W, pady=10, padx=10)
-    output = tk.Text(window, height=10, width=45)
-    output.grid(column=2, row=4, columnspan=3, pady=10, padx=10)
-    scroll = tk.Scrollbar(command=output.yview)
-    scroll.grid(column=5, row=4)
-    output.config(yscrollcommand=scroll.set)
-    statusbar = tk.Label(window,
-                         text="waiting for the url…",
-                         bd=1, relief=tk.SUNKEN,
-                         anchor=tk.W)
-    statusbar.grid(column=0,
-                    row=6,
-                    columnspan=5,
-                    sticky=tk.W+tk.E,
-                    pady=10,
-                    padx=10)
-    window.mainloop()
